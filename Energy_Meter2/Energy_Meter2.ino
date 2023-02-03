d80fe83c9b2ece3c19a62fa75656e0834a5c6bd4
johnbarbosas <89945583+johnbarbosas@users.noreply.github.com>
2023-02-03 01:14:31 -0300

    Update Energy_Meter2.ino

--- Energy_Meter2/Energy_Meter2.ino
+++ Energy_Meter2/Energy_Meter2.ino
@@ -39,39 +39,26 @@ void connect() {
   }
 
   Serial.println("\nconnected!");
-
-  client.subscribe("/hello");
-}
-
-void messageReceived(String &topic, String &payload) {
-  Serial.println("incoming: " + topic + " - " + payload);
-
-  // Note: Do not use the client in the callback to publish, subscribe or
-  // unsubscribe as it may cause deadlocks when other things arrive while
-  // sending and receiving acknowledgments. Instead, change a global variable,
-  // or push to a queue and handle it in the loop after calling `client.loop()`.
 }
 
 void setup() {
   Serial.begin(9600);
   WiFi.begin(ssid, pass);
-
-  // Note: Local domain names (e.g. "Computer.local" on OSX) are not supported
-  // by Arduino. You need to set the IP address directly.
-  client.begin("ec2-54-210-39-119.compute-1.amazonaws.com", net); //alterar para DNS de acesso
-  client.onMessage(messageReceived);
+  
+  client.begin("ec2-54-210-39-119.compute-1.amazonaws.com", net); // conexão ao servidor broker
 
   connect();
 }
 
 void loop() {
   client.loop();
-  //delay(10);  // <- fixes some issues with WiFi stability
 
   if (!client.connected()) {
     connect();
   }
-
+  
+  
+  // zerando os valores
   stringTensao = "";
   stringCorrente = "";
   stringPotencia = "";
@@ -81,41 +68,41 @@ void loop() {
   corrente = 0;
   correnteMedia = 0;
 
-  for (int j = 0; j<100; j++){
-     for (int i = 0; i<360; i++){
+  for (int j = 0; j<100; j++){ // 100 ciclos da senoide 
+     for (int i = 0; i<360; i++){ // leitura a cada grau da senoide
       
-        leituraT = analogRead(voltPin) - 727.5;
+        leituraT = analogRead(voltPin) - 727.5; // leitura da tensão e zerar o offset
         // Serial.print("Tensão: ");
         // Serial.println(leituraT);
         
-        if ((leituraT*leituraT) < 36) leituraT = 0;
+        if ((leituraT*leituraT) < 36) leituraT = 0; // exclusão do erro para pequenas tensões
 
-        leituraT *= VOLT_CAL;
-        leituraT *= leituraT;
-        tensao += leituraT;
+        leituraT *= VOLT_CAL; // valor de calibração
+        leituraT *= leituraT; // elevado ao quadrado
+        tensao += leituraT; // somatório das tensões
 
 
-        leituraC = analogRead(currentPin) - 230;
+        leituraC = analogRead(currentPin) - 230; // leitura da corrente e zerar o offset
         // Serial.print("Senoide:");
         // Serial.println(leituraC);
         
-        if ((leituraC*leituraC) < 36) leituraC = 0;
+        if ((leituraC*leituraC) < 36) leituraC = 0; // exclusão do erro para pequenas correntes
         
-        leituraC /= CURRENT_CAL;
-        leituraC *= leituraC;
-        corrente += leituraC;
+        leituraC /= CURRENT_CAL; // valor de calibração
+        leituraC *= leituraC; // elevado ao quadrado
+        corrente += leituraC; // somatório das correntes
         
-        delayMicroseconds(46.29);
+        delayMicroseconds(46.29); // 1s / 60Hz = 0,0166 / 360 = 46,29 us -> período da amostragem para 1 amostra por grau da senoide
       }
 
-    tensao /= 360;
-    tensao = sqrt(tensao);
+    tensao /= 360; // média da tensão
+    tensao = sqrt(tensao); // raiz quadrada -> valor em RMS
 
-    tensaoMedia += tensao;
-
-    corrente /= 360;
-    corrente = sqrt(corrente);
-
+    corrente /= 360; // média da corrrente
+    corrente = sqrt(corrente); // raiz quadrada -> valor em RMS
+    
+    // valores médios para não houver muita variação
+    tensaoMedia += tensao;  
     correnteMedia += corrente;
    
   }
@@ -125,32 +112,43 @@ void loop() {
   // Serial.println(tensaoMedia);
   
   correnteMedia /= 100;
-//  Serial.print("Corrente média: ");
-//  Serial.println(correnteMedia);
-    
-  stringTensao += tensaoMedia;
-  client.publish("/tensao", stringTensao);
+  // Serial.print("Corrente média: ");
+  // Serial.println(correnteMedia);
 
-  stringCorrente += correnteMedia;
-  client.publish("/corrente", stringCorrente);
-
-  correnteMedia = 200;
-  tensaoMedia = 220;
-
-  potencia = correnteMedia * tensaoMedia / 1000; 
+  potencia = correnteMedia * tensaoMedia / 1000; // cálculo da potência
   
-  energia += potencia * 1.66644 / 3600;
+  energia += potencia * 1.66644 / 3600; // a energia é a potência multiplica pelo tempo daquela potência, que nesse caso é 46,29 us * 60 * 100 / 3600 (3600 para o valor ficar em horas)
 
+  
+  // print dos valores medidos na porta serial
+  Serial.print("tensão: ");
+  Serial.print(tensaoMedia);
+  Serial.println(" V");
+  Serial.print("Corrente");
+  Serial.print(correnteMedia);
+  Serial.println(" A"); 
   Serial.print("Potencia: ");
   Serial.print(potencia);
   Serial.println(" kW");
   Serial.print("Energia: ");
   Serial.print(energia);
-  Serial.println(" kWh");  
+  Serial.println(" kWh"); 
+  
+  // salvando os valores como string para envia-lós ao broker
+    
+  // tensão
+  stringTensao += tensaoMedia;
+  client.publish("/tensao", stringTensao);
+  
+  // corrente
+  stringCorrente += correnteMedia;
+  client.publish("/corrente", stringCorrente);
 
+  // potência
   stringPotencia += potencia;
   client.publish("/potencia", stringPotencia);
-
+  
+  // energia
   stringEnergia += energia;
   client.publish("/energia", stringEnergia);
 }
